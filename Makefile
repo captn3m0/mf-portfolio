b83ce900ed2604247f4d2aa0292bee289516600b
Nemo <me@captnemo.in>
2025-01-20 20:13:54 +0530

    edelweiss, only URLs for now

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@ hdfc:
 	curl --silent "https://cms.hdfcfund.com/en/hdfc/api/v2/investor/weeklyDetails" \
   		--header 'referer: https://www.hdfcfund.com' | jq --raw-output '.data.filesData[]|.url' \
   		| wget --input-file=- --directory-prefix=raw_data/hdfc/ --no-clobber --no-verbose
+
+out/edelweiss.json:
+	node src/edelweiss.js
---
+++ src/edelweiss.js
@@ -0,0 +1,39 @@
+// This private key is published on the Edelweiss MF website
+const PRIVATE_KEY =
+  "-----BEGIN RSA PRIVATE KEY-----\n  MIIJJgIBAAKCAgBmqcBPfEaux/h3xfvjryZJEvHZWGBHGRNgaQy57dwaK2mX5CVY\n  s2uC6ETiVIkgU/hV5Uc9onPoFjgjVxn/wushO4Q4i0HqExYGvnB0UgM0tWtilELy\n  ytZOvRrKJcxPvt+OMzS6irN4G9+ceuMdTMtUoCTZ49cAD15ZH7zJGI3HRCG8dro2\n  ZXA5lRnOCpil3eEFtFj85RmycCNHiRiZ70APC2E7/N7Awz1231gd+DpZYSpHcNQ5\n  Hset5KpC91WL1a33DveMHLwWcNGfZhe3xxYIHkCchXvx1xfeK9C1MkdZpzE6Tvob\n  PRN4tZh0hvHdqgCM2utzssBH2kXipHr/AJcde3L65iyF6uoLtwHi76hwH6lnlXoU\n  tJ17h/zs6GZyGMw6XqhXe848FUX8QP+maF78aRdKw8fpwXkKmp55Ovu3Ix7tX05D\n  Z/Vdofcom962ZQE08/6b7FDJ7SW4suwNMkHqASz29XZdYaZwmx+hXqsUuIQ8whOH\n  JUiI4urFgUXu0Imj4918RSP4pbF+IYGdl69ArNT/3uYjt5/IDpc1gkq94+yHG/m7\n  8UHeexFUP3RHo6Qk9PdyQTC9Im3lOhZa5HrtUXab0GWMWRU2Hvajm0i8mb6re8DK\n  WN88HTxDvklTbrV+i2pxMzoGLv2yH/aL/KhVu4GX2CB2pB+MN6WpY8gGgQIDAQAB\n  AoICAF0N4oVphPvpv9p4t09yM8aqiFLUGzh2jyylK2bgJdAGp4iIwB09S+b1N3La\n  4wxNcFwTHVbNiDfG5A2MZsF/4PZXZwqFlzJfKGjmdzHIIKXHB1XsQx6lIkluePjf\n  9+gUf7f5zAKFtBTbVv0UGgY11FuZaK2jKf8g7wjjjo/MVL5YkCeuiizCF03qSuCK\n  lEBL9CnihYJ8+qTVpfSyXH0y8iJw0yq3kwSwM2crHEjV9bWML/FfYM2Bi8af7IAU\n  hhLglPXKODFk3kuM6USNGlR2/Q5QtYnqKCVWLIkxt9R1hVzLAgDzExpbaunP2CdP\n  Ja4g6KzDxgUx8b1vKEc9hUrcMxvbIMDhFArTog5rrSsP62rcyK2pD/ntNj9KmLjp\n  6TjWVWnjaczKF5wsuaA6EzHh2VfkX0gYDIgtzsaHhleWH43H4QGLAZagAKqiKa0v\n  IQa/7utASXorhpspRnNnzvS03wjc8SB3eAtNj1OwktSUBE0DlxHfSzUo1yi37yQk\n  Nzaa33iHi1Tem9zozQ+Fleu8pttWidiorRQwJ7YuaFTpXLtzR+Gt2j5ZuzfMkgE6\n  BRaPI6RkXp8hl4nxxSNZuZKVFRPSLo7sLUecvn1OAZpSLbRIgFS7s3lepum+bRIL\n  V+ft2AdGyWgvXrV8nFQzYnTTMT1TRu6LXybk0n1PSx0h2EUBAoIBAQC2PwFxcTU5\n  UK8IG8dt4UAWxhY7yjZVOsPsMRHa5OeJJpBL08m9ZZiYP7qbqrdf6En8/07N3W8F\n  vUioMCNRnuSAVmaMbhLDY/BngXEZE6krmdclXN1OFCWBE7skNEPZh5dhCxsEYFvV\n  u57L7UoXhmyPJHhHq6YOFJIzQS7tCWh0H1pksOLAdZ6TqAhEoqC0nZTXRP8X9QBr\n  n8fcdypAKTKCHWKbOlEoa9IFbuuAkKN3PAzlDZtt9ZKIK5YF1417/pkR7gb8v9wO\n  v8nkvkpglQyGsMJX8a2WHZlN/JgcgKbOWNwyVZko3rtrK859JYDMqwGMkd9+JDQf\n  mJbIXqTddaTRAoIBAQCQNdCIg0qglvy8kg0FraVZLCfjqbpDGNUKBYLKXw6uSXO3\n  zi3ShiQAfglC0WpIFr3g1HbsaSLSSRVbYuFd0rt+/Te40dUKd07eS2ZgO+gB1Wa7\n  OQiYhOJyFowJ4hmH4BjaEeHEA7ehg5VTRxc65nlrtei2FGj2OZbn9oWbRKCoUIkr\n  ljvG/A4XHR1m+Q4Nwtqd8VVS6JWB6bFvG3uslkTzeqDUhpCRAOVtyZ5IdiaE30N7\n  FUtibzkymv66a0jAz7BnBBV5+kTJ4x/wDITgmQkavoYGRCV6etHeTGcy6qzrqI8q\n  vx09ZAYYYNeMiIwvxzLeSm667yusQnHqt960THKxAoIBAGmHcFF9KUGoC8JZO5DP\n  1umZCHTUscOGhL6250zmMYBEeVe/KllmhAbdGDRdlVOO0ZETUVF5wHkFUxY40arI\n  pC4AWd9FOc40h5eX56KKtPHPYjp7XJaCqfjlMP9U2FkU375MzPcqyOKpgtk47SdU\n  KnZ0usVdiokqtkHBo+eUBA0BvrkBiDvDn/CGY0tDFGWhvfctN71qnDS1z+hEhI2k\n  jV+0nk2eZjJOxM1qninTFyTK+/iCeZchlRkOjA+T8pRzJ7WvLBKdElLkEYdH/Q+S\n  Hy+7GL4N7uM2/nefJYGTqYNhBxiKYma9Gq+DLU1gMwXQm9QG//AuDJSjKeSakUK/\n  hZECggEAN5DEvRT5mE8VKwOLiPiTkXx2Ej6/ykKzM51T++4MlhMvR09mUIvnK1lk\n  8B4IuaYpm4vCFu2bwTLvy7qZ47DjFdcztT8Eu3elJ/odhE8Q4kP7xYAAU+Q/7H3L\n  j7NS6wvqB3yEZSaZht5Qlzn5NXTArEP3WtEacGVYRCC08lLLJI+DiwvkFJPiSf2p\n  6pdkVuERo3Ht0nhTwy7sc0c/rAxqnPZPKhR/d5/Exzy8hGciCGiOc8qMtWztuRYH\n  uITGD6T273qfW9SW1MNy9ueb+358B5YJxjhTyJGyc9k0mmXaIyNbprjk51a3EoWe\n  CERE39P9QbGkYNrvFPy1MB+Q7B/YwQKCAQBlf8HYW4DApFUqXjTIR82syv9FXN9M\n  gCzkyMw37kqpm8JMXoFYwnCs/ZoGJlwc6HSE7ehUYhy3nBq3PnWtK3AKK6o5XeYA\n  T5yYkla4DEF8U0iRLajaTrnF3OfN0PE0qEpW2zjzgcinMKLwaRql7JCCtjRSc/3B\n  53HVxns9ay9M43NzhbMzpjF7oVtTr7y+4vd88t1gyahrxhcbeIYz/g251cZS2jBS\n  WuIJlpp5LHoTFUaPlhdEX39hb0zONcWaTF+/IaI2rYd1h97IygAJiHkyjTR6D8eY\n  j7XKBKg/KF5SFr9b+aM/RymwaLkbYImCX4zKl12UrxVwQaiPF61fCw42\n  -----END RSA PRIVATE KEY-----";
+// UNUSED
+// const SECRET_KEY = "5b6714126d3149fbab994747b2633287";
+
+var RSA = require("hybrid-crypto-js").RSA;
+var Crypt = require("hybrid-crypto-js").Crypt;
+
+var crypt = new Crypt();
+const URL = `https://api.edelweissmf.com/edelweissmf/api/v1/third-party/getSingleStatutory?menuid=2`;
+
+fetch(URL, {
+  method: "GET",
+  headers: {
+    "user-agent": "Chrome/999"
+  },
+})
+  .then(response => response.json())
+  .then(data => {
+    const cryptBox = data.body;
+    const decrypted = crypt.decrypt(PRIVATE_KEY, cryptBox).message;
+    
+      let rows = JSON.parse(decrypted)['CommonDetails'];
+      rows = rows.map((row) => {
+        return {Month: row['Month'],
+                Year: row['Year'],
+                FileTitle: row['FileTitle'],
+                FilePath: row['FilePath']}
+      })
+
+      // Write rows to out/edelweiss.json
+      const fs = require('fs');
+      fs.writeFileSync('out/edelweiss.json', JSON.stringify(rows, null, 2));  
+    
+  })
+  .catch(error => {
+    console.error('Error processing request:', error);
+  });
